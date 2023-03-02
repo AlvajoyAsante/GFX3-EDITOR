6b4dd9a2340a43a9e85ab5724a7948be0c02de0f
Alvajoy Asante <33734347+Overload02@users.noreply.github.com>
2023-03-01 20:42:49 -0500

    gitignore test

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ *--------------------------------------
+ * Program Name: GFX3E
+ * Author: ALVAJOY ASANTE
+ * License: AGPL v3.0
+ * Description: A sprite stacking editor
+ * "Please ask for permission before any use of Authors code!"
+ *--------------------------------------
+*/
+
+#include <tice.h>
+#include <graphx.h>
+#include <keypadc.h>
+
+void main(void)
+{
+
+}
